c9c078bc1a7ea74775458f403bae6f653d6477df
Kiana Aghaei hashtjin <kiana@Kianas-MacBook-Air.local>
2016-11-23 06:39:02 -0800

    about page css

--- themes/inhabitent/about.php
+++ themes/inhabitent/about.php
@@ -1,12 +1,12 @@
 <?php /* Template Name: About Page */ ?>
 <?php get_header(); ?>
-
-<div id="primary" class="content-area">
-  <main id="main" class="site-main">
-    <section class="about-hero">
+ <section class="about-hero">
       <h1><?php the_title(); ?></h1>
     </section>
-    <section class="about-wrap">
+<div id="primary" class="content-area">
+  <main id="main" class="site-main">
+   
+    <section class="about-text">
       <h2>Our Story</h2>
       <?php echo CFS()->get( 'our_story' ); ?>
       <h2>Our Team</h2>
@@ -15,3 +15,4 @@
   </main><!-- #main -->
 </div><!-- #primary -->
 <?php get_footer(); ?>
+
