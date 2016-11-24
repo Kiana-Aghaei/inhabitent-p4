c1fa9426f6fb0d14267f6aaba7246f24b627e189
Kiana Aghaei hashtjin <kiana@Kianas-MacBook-Air.local>
2016-11-24 04:49:57 -0800

    front page shop css

--- themes/inhabitent/taxonomy-product_type.php
+++ themes/inhabitent/taxonomy-product_type.php
@@ -1,40 +1,44 @@
-<?php get_header(); ?>
-
-<div id="primary" class="content-area product-area">
-
+<?php
+/**
+ * The template for displaying archive product type pages.
+ *
+ * @package RED_Starter_Theme
+ */
+get_header(); ?>
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<?php if ( have_posts() ) : ?>
-
+			<?php if ( have_posts() ) : ?>
 			<header class="page-header">
 				<?php
-					 the_archive_title( '<h1 class="page-title">', '</h1>') ;
+					the_archive_title( '<h2 class="page-title">', '</h2>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
-    <section class= "all-products">
-
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-            
-                <div class = "one-product">
-				    <?php
-					    get_template_part( 'template-parts/content-products' );
-				    ?>
-                
-                </div>
-              
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content-products', 'none' ); ?>
-
-		<?php endif; ?>
-    </section>  
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_footer(); ?>
+			</header>
+			<!-- .page-header -->
+			<div class="boxes" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<?php while ( have_posts() ) : the_post(); ?>
+				<div class="product-post">
+					<div class="product-thumbnail">
+						<a href="<?php the_permalink() ; ?> ">
+							<?php the_post_thumbnail( 'large' ); ?>
+						</a>
+					</div>
+					<div class="product-info">
+						<?php the_title('<p class="product-title">'); ?>........
+						<?php echo CFS()->get( 'product_price' ); ?>
+					</div>
+				</div>
+				<!-- product-post -->
+				<?php endwhile ?>
+			</div>
+	</div>
+	<!-- #post-## -->
+	</div>
+	<?php else : ?>
+	<?php get_template_part( 'template-parts/content', 'none' ); ?>
+	<?php endif; ?>
+	</main>
+	<!-- #main -->
+	</div>
+	<!-- #primary -->
+	<?php get_footer(); ?>
